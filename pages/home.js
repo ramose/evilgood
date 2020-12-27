4059bc34c42f2290e671f7965e0e8cf28d32164b
Leonard Papilaya <leopapilaya@gmail.com>
2020-12-27 12:13:40 +0800

    Time stat percentage bar.

--- pages/home.js
+++ pages/home.js
@@ -1,19 +1,147 @@
-import React from "react";
-import {View,Text, StyleSheet} from "react-native";
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  SafeAreaView,
+  Button,
+  ScrollView,
+} from 'react-native';
+// import {ScrollView} from 'react-native-gesture-handler';
+import mainStyle from '../styles/mainstyle.js';
+import data from '../assets/data.json';
+import {useState} from 'react';
 
-export default function Home(){
-    return(
-        <View style={styles.container}>
-            <Text>Home Ready...</Text>
-        </View>
-    )
+export default function Home() {
+  const [evilValue, setEvilValue] = useState('90%');
+  const [goodValue, setGoodValue] = useState('10%');
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={{padding: 20}}>
+        <Header />
+        <TimeMenus evil={evilValue} good={goodValue} />
+        
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
+export function Header() {
+  return (
+    <View style={{marginBottom: 40}}>
+      <Text style={{marginBottom: 20}}>Dec, 20 2020</Text>
+      <View style={{flexDirection: 'row'}}>
+        <Image source={require('../assets/dashboard/user.png')} />
+        <View style={{paddingLeft: 10, justifyContent: 'center'}}>
+          <Text style={mainStyle.title2}>Hello, {data.username}</Text>
+          <Text>Welcome Back</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const TimeMenus = (props) => {
+  return (
+    <View>
+      <Text style={mainStyle.title1}>You Are</Text>
+
+      <View
+        style={{
+          flexDirection: 'row',
+        }}>
+        <ScrollView horizontal={true}>
+          <Button color="black" title="Today" onPress={() => {}} />
+          <Button color="black" title="This Week" />
+          <Button color="black" title="This Month" />
+          <Button color="black" title="This Year" />
+          <Button color="black" title="All Time" />
+        </ScrollView>
+      </View>
+
+      <View style={{flexDirection: 'row', borderRadius: 20}}>
+        <View
+          style={{
+            backgroundColor: 'red',
+            width: props.evil,
+            height: 100,
+            // borderTopLeftRadius: 30,
+            // borderBottomLeftRadius: 30,
+            justifyContent: 'center',
+            paddingLeft: 20,
+          }}>
+          <View
+            style={{
+              position: 'absolute',
+              zIndex: 10,
+              left: 10,
+              justifyContent: 'center',
+            }}>
+            <Text style={mainStyle.bigtitle1}>{props.evil}</Text>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Image source={require('../assets/dashboard/lovew.png')} />
+              <Text
+                style={{
+                  color: 'white',
+                  fontSize: 20,
+                  fontWeight: 'bold',
+                  paddingLeft: 5,
+                }}>
+                Evil
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View
+          style={{
+            backgroundColor: '#7EBE35',
+            width: props.good,
+            // height: 100,
+            // borderTopRightRadius: 30,
+            // borderBottomRightRadius: 30,
+            justifyContent: 'center',
+            paddingRight: 20,
+          }}></View>
+        <View
+          style={{
+            position: 'absolute',
+            zIndex: 10,
+            right: 10,
+            justifyContent: 'center',
+            height:100
+          }}>
+          <Text style={mainStyle.bigtitle1}>{props.good}</Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+            }}>
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 20,
+                fontWeight: 'bold',
+                paddingRight: 5,
+              }}>
+              Good
+            </Text>
+            <Image source={require('../assets/dashboard/angelw.png')} />
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center",
-        backgroundColor:"white"        
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  button: {
+    color: 'black',
+  },
+});
---
+++ styles/mainstyle.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from "react-native";
+
+const mainStyle = StyleSheet.create({
+    bigtitle1:{
+        fontSize:46,
+        fontWeight:"bold",
+        color:"white"
+    },
+    title1:{
+        fontSize:26,
+        fontWeight:"bold"
+    },
+    title2:{
+        fontSize:20,
+        fontWeight:"bold"
+    },
+    subtitle:{
+        fontSize:18,
+        fontWeight:"bold"
+    }
+})
+
+export default mainStyle;
